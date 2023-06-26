b1f1c1a4493e0e10ab7ae3c15f894ef90437d6e2
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-06-26 15:58:49 +0530

    add js-switch-case-using-get-the-days-of-the-week

---
+++ main.js
@@ -0,0 +1,29 @@
+// 1- Get the days of the week using switch case
+
+let days = parseInt(prompt("Enter a weekdays:"));
+switch (days) {
+    case 1:
+        console.log('Sunday');
+        break;
+    case 2:
+        console.log('Monday');
+        break;
+    case 3:
+        console.log('Tuesday');
+        break;
+    case 4:
+        console.log('Wednesday');
+        break;
+    case 5:
+        console.log('Thursday');
+        break;
+    case 6:
+        console.log('Friday');
+        break;
+    case 7:
+        console.log('Saturday');
+        break;
+    default:
+        console.log('no days')
+        break;
+}
